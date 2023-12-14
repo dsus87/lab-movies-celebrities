a014a462fc0db054c0b43d2ebed90395855e6622
Daniel Susetio <dsusetio87@gmail.com>
2023-12-14 14:09:52 +0100

    iteration 1-3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,3 +1,7 @@
+const express = require('express');
+
+const Celebrity = require('../models/Celebrity.model')
+
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
@@ -10,14 +14,17 @@ router.get('/celebrities/create', (req, res) => {
   });
 
 // Route to process the form and create a new celebrity
-router.post('/celebrities/create', async (req, res) => {
-    try {
-      const { name, occupation, catchPhrase } = req.body;
-      await Celebrity.create({ name, occupation, catchPhrase });
-      res.redirect('/celebrities'); // Redirect to the list of celebrities, to be implemented in the next iteration
-    } catch (error) {
+router.post('/celebrities/create', (req, res, next) => {
+  console.log('Creating a celebrity with:', req.body);
+  const { name, occupation, catchPhrase } = req.body;
+
+  Celebrity.create(req.body)
+    .then(() => res.redirect('/celebrities')) // Redirect to the list of celebrities
+    .catch(err => {
+      console.error('Error creating a new celebrity:', err); // Log the error for debugging
       res.render('celebrities/new-celebrity', { errorMessage: 'Error creating a new celebrity' });
-    }
-  });
+    });
+});
+  
 
 module.exports = router;
